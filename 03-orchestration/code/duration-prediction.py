d34376f18e68e3d55c5b2ba05aa955176acb1783
Raman Jafarau <romandzhafarov95@gmail.com>
2026-07-08 20:36:26 +0000

    feat: Update `duration-prediction.py`

--- 03-orchestration/code/duration-prediction.py
+++ 03-orchestration/code/duration-prediction.py
@@ -1,46 +1,43 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import argparse
+import pathlib
 import pickle
-from pathlib import Path
 
+import mlflow
 import pandas as pd
 import xgboost as xgb
-
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.metrics import root_mean_squared_error
 
-import mlflow
-
-mlflow.set_tracking_uri("http://localhost:5000")
 mlflow.set_experiment("nyc-taxi-experiment")
 
-models_folder = Path('models')
+models_folder = pathlib.Path("models")
 models_folder.mkdir(exist_ok=True)
 
 
-
 def read_dataframe(year, month):
-    url = f'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_{year}-{month:02d}.parquet'
+    url = f"https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_{year}-{month:02d}.parquet"
     df = pd.read_parquet(url)
 
-    df['duration'] = df.lpep_dropoff_datetime - df.lpep_pickup_datetime
-    df.duration = df.duration.apply(lambda td: td.total_seconds() / 60)
+    df["duration"] = df["lpep_dropoff_datetime"] - df["lpep_pickup_datetime"]
+    df["duration"] = df["duration"].dt.total_seconds() / 60
 
-    df = df[(df.duration >= 1) & (df.duration <= 60)]
+    df = df[(df["duration"] >= 1) & (df["duration"] <= 60)]
 
-    categorical = ['PULocationID', 'DOLocationID']
+    categorical = ["PULocationID", "DOLocationID"]
     df[categorical] = df[categorical].astype(str)
 
-    df['PU_DO'] = df['PULocationID'] + '_' + df['DOLocationID']
+    df["PU_DO"] = df["PULocationID"] + "_" + df["DOLocationID"]
 
     return df
 
 
 def create_X(df, dv=None):
-    categorical = ['PU_DO']
-    numerical = ['trip_distance']
-    dicts = df[categorical + numerical].to_dict(orient='records')
+    categorical = ["PU_DO"]
+    numerical = ["trip_distance"]
+    dicts = df[categorical + numerical].to_dict(orient="records")
 
     if dv is None:
         dv = DictVectorizer(sparse=True)
@@ -52,18 +49,19 @@ def create_X(df, dv=None):
 
 
 def train_model(X_train, y_train, X_val, y_val, dv):
+    mlflow.xgboost.autolog(disable=True)
     with mlflow.start_run() as run:
         train = xgb.DMatrix(X_train, label=y_train)
         valid = xgb.DMatrix(X_val, label=y_val)
 
         best_params = {
-            'learning_rate': 0.09585355369315604,
-            'max_depth': 30,
-            'min_child_weight': 1.060597050922164,
-            'objective': 'reg:linear',
-            'reg_alpha': 0.018060244040060163,
-            'reg_lambda': 0.011658731377413597,
-            'seed': 42
+            "max_depth": 44,
+            "learning_rate": 0.1719747846289316,
+            "reg_alpha": 4.896614873911946e-05,
+            "reg_lambda": 0.019446810721081013,
+            "min_child_weight": 1.785047439733004,
+            "objective": "reg:squarederror",
+            "random_state": 42,
         }
 
         mlflow.log_params(best_params)
@@ -71,20 +69,22 @@ def train_model(X_train, y_train, X_val, y_val, dv):
         booster = xgb.train(
             params=best_params,
             dtrain=train,
-            num_boost_round=30,
-            evals=[(valid, 'validation')],
-            early_stopping_rounds=50
+            num_boost_round=1000,
+            evals=[(valid, "validation")],
+            early_stopping_rounds=50,
         )
 
-        y_pred = booster.predict(valid)
-        rmse = root_mean_squared_error(y_val, y_pred)
-        mlflow.log_metric("rmse", rmse)
+        y_pred = booster.predict(
+            data=valid, iteration_range=(0, booster.best_iteration + 1)
+        )
+        error = root_mean_squared_error(y_val, y_pred)
+        mlflow.log_metric("error", error)
 
-        with open("models/preprocessor.b", "wb") as f_out:
+        with open("models/preprocessor.pkl", "wb") as f_out:
             pickle.dump(dv, f_out)
-        mlflow.log_artifact("models/preprocessor.b", artifact_path="preprocessor")
+        mlflow.log_artifact("models/preprocessor.pkl", artifact_path="preprocessor")
 
-        mlflow.xgboost.log_model(booster, artifact_path="models_mlflow")
+        mlflow.xgboost.log_model(booster, name="models_mlflow")
 
         return run.info.run_id
 
@@ -99,7 +99,7 @@ def run(year, month):
     X_train, dv = create_X(df_train)
     X_val, _ = create_X(df_val, dv)
 
-    target = 'duration'
+    target = "duration"
     y_train = df_train[target].values
     y_val = df_val[target].values
 
@@ -109,11 +109,15 @@ def run(year, month):
 
 
 if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser(description='Train a model to predict taxi trip duration.')
-    parser.add_argument('--year', type=int, required=True, help='Year of the data to train on')
-    parser.add_argument('--month', type=int, required=True, help='Month of the data to train on')
+    parser = argparse.ArgumentParser(
+        description="Train a model to predict taxi trip duration."
+    )
+    parser.add_argument(
+        "--year", type=int, required=True, help="Year of the data to train on"
+    )
+    parser.add_argument(
+        "--month", type=int, required=True, help="Month of the data to train on"
+    )
     args = parser.parse_args()
 
     run_id = run(year=args.year, month=args.month)
