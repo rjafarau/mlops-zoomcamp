f6309d70c2c5c7a2ee25298d62b48f0099e25cda
Raman Jafarau <romandzhafarov95@gmail.com>
2026-09-19 00:06:27 +0300

    feat: Update `score_backfill.py`

--- 04-deployment/batch/score_backfill.py
+++ 04-deployment/batch/score_backfill.py
@@ -7,21 +7,20 @@ import score
 
 
 @flow
-def ride_duration_prediction_backfill():
-    start_date = datetime(year=2021, month=3, day=1)
-    end_date = datetime(year=2022, month=4, day=1)
-
+def run(start_date, end_date):
     d = start_date
-
     while d <= end_date:
-        score.ride_duration_prediction(
-            taxi_type='green',
-            run_id='e1efc53e9bd149078b0c12aeaa6365df',
-            run_date=d
+        score.run(
+            taxi_type="green",
+            year=d.year,
+            month=d.month,
+            model_id="m-205aad2b19454838af2cfe5644624f7e",
         )
-
         d = d + relativedelta(months=1)
 
 
-if __name__ == '__main__':
-    ride_duration_prediction_backfill()
+if __name__ == "__main__":
+    run(
+        start_date=datetime(year=2021, month=3, day=1),
+        end_date=datetime(year=2022, month=4, day=1),
+    )
